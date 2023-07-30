d882a7935dea05fa0684222cd04b6f039a0cbe02
nadkon <nadkon@gmail.com>
2023-07-30 22:16:51 +0300

    2023-07-13: task with password generation completed

---
+++ 2023-07-13/homework/password.py
@@ -0,0 +1,17 @@
+'''
+Напишіть програму, яка імпортує модуль string та модуль random. Використовуйте функції цих модулів для генерації випадкового паролю. Попросіть користувача ввести довжину паролю, а потім використайте функцію random.choice для вибору випадкових символів з рядка string.ascii_letters + string.digits. Згенерований пароль виведіть на екран.
+'''
+
+import string, random
+
+def password_generator():
+    try:
+        entry = int(input('Enter the lengh of the password: '))
+        password = ''
+        while len(password) < entry:
+            password += random.choice(string.ascii_letters + string.digits)
+        return password
+    except ValueError:
+            return('It is not a digit')
+
+print(password_generator())
---
+++ 2023-07-13/module/main.py
@@ -0,0 +1,9 @@
+import mymodule
+import pandas as pd
+
+mymodule.greet('John')
+print(mymodule.square(5))
+
+df = pd.DataFrame([[1, 2, 3], [4, 5, 6]])
+
+print(df)
