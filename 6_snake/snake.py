7401abbd830594580b799e07ebfc5174d14922a2
nadkon <nadkon@gmail.com>
2023-08-01 14:11:00 +0300

    snake: some changes in movement

--- 6_snake/snake.py
+++ 6_snake/snake.py
@@ -7,6 +7,7 @@ class Snake:
         self.x = x
         self.y = y
         self.head = pg.Rect(self.x, self.y, SQUARE_WIDTH, SQUARE_HEIGHT)
+       # self.tail = pg.Rect(self.x, self.y, SQUARE_WIDTH, SQUARE_HEIGHT)
 
     def draw(self, screen):
         screen.fill('purple')
@@ -14,7 +15,8 @@ class Snake:
 
     def move(self, key):
         if key == pg.K_UP:
-            self.y -= SQUARE_HEIGHT
+            if self.y > 5:
+                self.y -= SQUARE_HEIGHT
         if key == pg.K_DOWN:
             self.y += SQUARE_HEIGHT
         if key == pg.K_RIGHT:
